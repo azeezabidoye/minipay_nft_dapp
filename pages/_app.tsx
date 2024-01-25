97115fb20c1ece60965968e2df0f19a5e6be58c9
azeezabidoye <azeezabidoye@yahoo.com>
2024-01-25 14:39:38 +0100

    first commit

---
+++ pages/_app.tsx
@@ -0,0 +1,42 @@
+import { Alfajores, Celo } from "@celo/rainbowkit-celo/chains";
+import celoGroups from "@celo/rainbowkit-celo/lists";
+import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import "@rainbow-me/rainbowkit/styles.css";
+import type { AppProps } from "next/app";
+import { WagmiConfig, configureChains, createConfig } from "wagmi";
+import { publicProvider } from "wagmi/providers/public";
+import Layout from "../components/Layout";
+import "../styles/globals.css";
+import { InjectedConnector } from "wagmi/connectors/injected";
+
+// const projectId = process.env.NEXT_PUBLIC_WC_PROJECT_ID as string; // get one at https://cloud.walletconnect.com/app
+
+const { chains, publicClient } = configureChains(
+  [Celo, Alfajores],
+  [publicProvider()]
+);
+
+const connectors = [new InjectedConnector({ chains })];
+
+const appInfo = {
+  appName: "Celo Composer",
+};
+
+const wagmiConfig = createConfig({
+  connectors,
+  publicClient: publicClient,
+});
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider chains={chains} appInfo={appInfo} coolMode={true}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
+}
+
+export default App;
